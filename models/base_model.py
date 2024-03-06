e7d282278e502e5c2b7632c2cd9477b0a31106a8
Hector Macassa <cbmacassa@gmail.com>
2024-03-06 15:35:11 -0800

    BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""This module defines a Base class for all models"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    This class represents a base class for all models
+
+    Attributes:
+        id (str): Unique indentifier for the instances.
+        created_at (datetime): Time for when instance is created.
+        updated_at (datetime): Time when instance was last updated.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the BaseModel class.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Returns a string representation of the instance.
+
+        Returns:
+            str: String representation of the instance.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Updates the updated_at attribute with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the instance.
+
+        Returns:
+            dict: Dictionary representation of the instance.
+        """
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from models.base_model import BaseModel
 
 
-class TestBaseModel(untitest.TestCase):
+class TestBaseModel(unittest.TestCase):
     """
     Test cases for the BaseModel class.
     """
@@ -26,3 +26,34 @@ class TestBaseModel(untitest.TestCase):
         self.assertIsInstance(self.model.created_at, datetime)
         self.assertIsInstance(self.model.updated_at, datetime)
         self.assertNotEqual(self.model.created_at, self.model.updated_at)
+
+    def test_str(self):
+        """
+        Test the string representation of a BaseModel instance.
+        """
+        expected_str = f"[BaseModel] ({self.model.id}) {self.model.__dict__}"
+        self.assertEqual(str(self.model), expected_str)
+
+    def test_save(self):
+        """
+        Test the save method of a BaseModel instance.
+        """
+        old_updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(old_updated_at, self.model.updated_at)
+
+    def test_to_dict(self):
+        """
+        Tests the to_dict method of a BaseModel instance.
+        """
+        model_dict = self.model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertIn("__class__", model_dict)
+        self.assertEqual(model_dict["__class__"], "BaseModel")
+        self.assertIn("created_at", model_dict)
+        self.assertIsInstance(model_dict["created_at"], str)
+        self.assertIn("updated_at", model_dict)
+        self.assertIsInstance(model_dict["updated_at"], str)
+
+if __name__ == "__main__":
+    unittest.main()
