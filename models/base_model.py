bf2e4aaf4881a96071ee5b287a5f8a4980bda898
Hector Macassa <cbmacassa@gmail.com>
2024-03-09 09:49:53 -0800

    Modified base_model

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module defines a Base class for all models"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -32,6 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -47,6 +49,7 @@ class BaseModel:
         Updates the updated_at attribute with the current datetime.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
