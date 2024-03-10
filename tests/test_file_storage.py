a470b2afc055d7265820866c683f0dac422334c1
Hector Macassa <cbmacassa@gmail.com>
2024-03-09 21:19:10 -0800

    Added FileStorage test

---
+++ tests/test_file_storage.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+This module defines tests forFileStorage class
+"""
+import unittest
+import os
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+    Test cases for FileStorage class
+    """
+
+    def setUp(self):
+        """
+        Set up a new  instance of the FileStorage class for each test case
+        """
+        self.storage = FileStorage()
+
+    def tearDown(self):
+        """
+        Remove the JSON file after each test case
+        """
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_all(self):
+        """
+        Test the all  method of the FileStorage class
+        """
+        self.assertIsInstance(self.storage.all(), dict)
+
+    def test_new(self):
+        """
+        Test the new method of the FileStorage class
+        """
+        model = BaseModel()
+        self.storage.new(model)
+        key = f"{model.__class__.__name__}.{model.id}"
+        self.assertIn(key, self.storage.all().keys())
+
+    def test_save(self):
+        """
+        Test the save method of the FileStorage class
+        """
+        model = BaseModel()
+        self.storage.new(model)
+        self.storage.save()
+        self.assertTrue(os.path.exists("file.json"))
+
+    def test_reload(self):
+        """
+        Test reload method of the FileStorage class
+        """
+        model = BaseModel()
+        self.storage.new(model)
+        self.storage.save()
+        self.storage.reload()
+        key = f"{model.__class__.__name__}.{model.id}"
+        self.assertIn(key, self.storage.all().keys())
+
+
+if __name__ == "__main__":
+    unittest.main()
